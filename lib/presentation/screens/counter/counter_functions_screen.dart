50b4472cbb04845a5d833495c211cc54d2bcf2cd
alextina <castilloavilaa@gmail.com>
2025-01-23 18:02:54 -0500

    [style] Customize widgets: added padding, updated colors, and adjusted layout

--- lib/presentation/screens/counter/counter_functions_screen.dart
+++ lib/presentation/screens/counter/counter_functions_screen.dart
@@ -15,16 +15,29 @@ class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Counter Functions'),
+          title: Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Text(
+                'Counter Functions',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.w900),
+              )),
           actions: [
-            IconButton(
+            Padding(
+              padding: EdgeInsets.only(right: 10),
+              child: IconButton(
                 onPressed: () {
                   setState(() {
                     clickCounter = 0;
                   });
                 },
-                icon: Icon(Icons.refresh_rounded))
+                icon: Icon(Icons.refresh_rounded),
+                color: Colors.white,
+                iconSize: 30,
+              ),
+            )
           ],
+          backgroundColor: Colors.deepPurple,
         ),
         body: Center(
           child: Column(
@@ -35,36 +48,42 @@ class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
                 style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
               ),
               Text(
-                'Click${clickCounter == 1 ? '' : 's'}',
+                'click${clickCounter == 1 ? '' : 's'}',
                 style: TextStyle(fontSize: 25),
               )
             ],
           ),
         ),
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            CustomButton(
-              icon: Icons.plus_one,
-              onPressed: () {
-                setState(() {
-                  clickCounter++;
-                });
-              },
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            CustomButton(
-              icon: Icons.exposure_minus_1,
-              onPressed: () {
-                if (clickCounter == 0) return;
-                setState(() {
-                  clickCounter--;
-                });
-              },
-            )
-          ],
+        floatingActionButton: Padding(
+          padding: EdgeInsets.only(bottom: 20, left: 30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: CustomButton(
+                  icon: Icons.exposure_minus_1,
+                  onPressed: () {
+                    if (clickCounter == 0) return;
+                    setState(() {
+                      clickCounter--;
+                    });
+                  },
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                  child: CustomButton(
+                icon: Icons.plus_one,
+                onPressed: () {
+                  setState(() {
+                    clickCounter++;
+                  });
+                },
+              )),
+            ],
+          ),
         ));
   }
 }
