7a464935c455b668a880671b3ae07979826c3aed
alextina <castilloavilaa@gmail.com>
2025-01-23 16:31:55 -0500

    [feat] Change the state of the application and the word click depending on this state

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -12,18 +19,23 @@ class CounterScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              '10',
+              '$clickCounter',
               style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
             ),
             Text(
-              'Clicks',
+              'Click${clickCounter == 1 ? '' : 's'}',
               style: TextStyle(fontSize: 25),
             )
           ],
         ),
       ),
-      floatingActionButton:
-          FloatingActionButton(onPressed: () {}, child: Icon(Icons.plus_one)),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              clickCounter++;
+            });
+          },
+          child: Icon(Icons.plus_one)),
     );
   }
 }
